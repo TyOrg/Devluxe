57366b5b93a4babe0abefc068cac89ca4dec11ff
temoty <temoty@hotmail.com>
2017-02-07 23:37:38 -0500

    added Bootstrap to this cutome theme.

--- functions.php
+++ functions.php
@@ -1,13 +1,30 @@
 <?php 
 
-function devluxe_script_enqueue() {
+/*
+	==============================
+	Include scripts
+	==============================
+*/
 
-wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/devluxe.css', array(), '1.0.0', 'all');
-wp_enqueue_script('customjs', get_template_directory_uri() . '/js/devluxe.js', array(), '1.0.0', 'true');
+function devluxe_script_enqueue() {
+	//css
+	wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/devluxe.css', array(), '1.0.0', 'all');
+	wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '3.3.4', 'all');
+
+	//js
+	wp_enqueue_script('jquery');
+	wp_enqueue_script('customjs', get_template_directory_uri() . '/js/devluxe.js', array(), '1.0.0', 'true');
+	wp_enqueue_script('bootstrapjs', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '3.3.4', 'true');
 }
 
 
 add_action( 'wp_enqueue_scripts', 'devluxe_script_enqueue');
+
+/*
+	==============================
+	Activate Menus
+	==============================
+*/
 
 function devluxe_theme_setup() {
 	add_theme_support('menus');
