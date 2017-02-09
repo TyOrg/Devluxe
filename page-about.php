feef2f61c0b882a39796ad2648aa6b74c43253c8
temoty <temoty@hotmail.com>
2017-02-08 23:16:05 -0500

    Created home-page.php as a custome home page.

--- page-about.php
+++ page-about.php
@@ -1,5 +1,9 @@
 <?php get_header(); ?>
 
+<div class="row">
+
+  <div class="col-xs-12 col-sm-8">
+
 	<?php 
 		if( have_posts() ):
 		
@@ -16,6 +20,14 @@
 	endif;
 	
 	?>
+  </div>
+
+
+	<div class="col-xs-12 col-sm-4">
+		<?php get_sidebar(); ?>
+  </div>
+
+</div>
 
 <?php get_footer(); ?>
 
