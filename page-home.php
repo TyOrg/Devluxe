33c46d3a126dbafd9b85f9d1fa9afc238b4456fa
temoty <temoty@hotmail.com>
2017-03-14 23:43:19 -0400

    closed h1 tag in page-home.php.

--- page-home.php
+++ page-home.php
@@ -44,7 +44,9 @@
 								      		<!-- Removed hyperlink from Carousel Titles -->
 									      <?php/* the_title( sprintf('<h1 class="entry-title"><a href="%s">', esc_url( get_permalink() ) ),'</a></h1>' ); */?>
 
-									      <?php the_title('<h1 class="entry-title">'); ?>
+                                          
+                                          <?php the_title( sprintf('<h1 class="entry-title">'), '</h1>'); ?>
+
 			
 										  <small><?php/* the_category(' '); */?></small>
 								      </div>
