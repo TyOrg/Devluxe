01e35f59a1023bfc44fe96ee25b924e722f4ea24
temoty <temoty@hotmail.com>
2017-03-17 16:54:07 -0400

    removed code brackets from contact form shortcode and removed top margin on portfolio pieces.

--- 1content-archive.php
+++ 1content-archive.php
@@ -1,7 +1,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<header class="entry-header">
-        <h1><a href="http://<?php the_title() ?>"><?php the_title() ?></a></h1>
+		<?php the_title( sprintf('<h1 class="entry-title"><a href="%s">', esc_url( get_permalink() ) ),'</a></h1>' ); ?>
 <!--<small>Posted n: <?php the_time('F j, Y'); ?> at <?php the_time('g:i a'); ?>, in <?php the_category(); ?></small>-->
 	</header>
 
@@ -10,7 +10,7 @@
 		<?php if( has_post_thumbnail() ): ?>
 
 		<div class="col-xs-12 col-sm-4">
-            <div class="thumbnail"><a href="http://<?php the_title() ?>"><?php the_post_thumbnail('medium'); ?></a></div>
+			<div class="thumbnail"><?php the_post_thumbnail('medium'); ?></div>
 		</div>
 		<div class="col-xs-12 col-sm-offset-1 col-sm-4 col-sm-offset-3">
 			<?php the_excerpt(); ?>
