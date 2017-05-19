de628790caec4928355cde222316fca6551ccc3e
temoty <temoty@hotmail.com>
2017-05-19 00:20:54 -0400

    Created UL with empty li to create bullet effect for Experience list.

--- page-home.php
+++ page-home.php
@@ -43,178 +43,193 @@
 
         <!--<div class="column column-50 column-offset-25">-->
 
- <!-- .row .column.column-offset-25 {
+        <!-- .row .column.column-offset-25 {
   margin-left: 25%;
 } -->
 
         <div class="container">
             <section class="top-background-color">
-            <div class="row">
-                
-                <div class="column column-offset-20 heading">
-                    <p>
-                        <?php the_field('heading1'); ?>
-                    </p>
-                   
-                </div>
-                
+                <div class="row">
 
-            </div>
-            <div class="row">
-                
-
-
-                <div class="column column-offset-25">
-                    <div class="row">
-
-
-                        <figure id="profile-photo" class="column column-25"><img src="<?php the_field('facephoto'); ?>"></figure>
-
-
-                        <ul id="ul-style" class="column column-75" style="list-style: none;">
-
-                            <li><i class="fa fa-globe" aria-hidden="true"></i>&nbsp;&nbsp;Boston</li>
-                            <li><i class="fa fa-envelope-o" aria-hidden="true"></i>&nbsp;&nbsp;tylercornettnow [at] gmail.com</li>
-                            <li><i class="fa fa-envelope-o" aria-hidden="true"></i>&nbsp;&nbsp;Twitter</li>
-
-                        </ul>
+                    <div class="column column-offset-20 heading">
+                        <p>
+                            <?php the_field('heading1'); ?>
+                        </p>
 
                     </div>
+
+
                 </div>
-                
-            </div>
-            <div class="row">
-                
-                
-                <div class="column column-30 column-offset-25">
-                    <p><i>
+                <div class="row">
+
+
+
+                    <div class="column column-offset-25">
+                        <div class="row">
+
+
+                            <figure id="profile-photo" class="column column-25"><img src="<?php the_field('facephoto'); ?>"></figure>
+
+
+                            <ul id="ul-style" class="column column-75" style="list-style: none;">
+
+                                <li><i class="fa fa-globe" aria-hidden="true"></i>&nbsp;&nbsp;Boston</li>
+                                <li><i class="fa fa-envelope-o" aria-hidden="true"></i>&nbsp;&nbsp;tylercornettnow [at] gmail.com</li>
+                                <li><i class="fa fa-envelope-o" aria-hidden="true"></i>&nbsp;&nbsp;Twitter</li>
+
+                            </ul>
+
+                        </div>
+                    </div>
+
+                </div>
+                <div class="row">
+
+
+                    <div class="column column-30 column-offset-25">
+                        <p><i>
                         Front End Developer
                         </i>
-                    </p>
+                        </p>
+
+                    </div>
+                    <div class="column">
+                        <!-- <hr class="hr"> add this only on desktop screens -->
+
+                    </div>
 
                 </div>
-                <div class="column">
-                    <!-- <hr class="hr"> add this only on desktop screens -->
+                <div class="row">
+
+
+                    <div class="column column-50 column-offset-25">
+                        <p>
+                            <?php the_content(); ?>
+                        </p>
+
+                    </div>
+
 
                 </div>
-                
-            </div>
-            <div class="row">
-                
-                
-                <div class="column column-50 column-offset-25">
-                    <p>
-                        <?php the_content(); ?>
-                    </p>
 
-                </div>
-                          
-               
-            </div>
-          
             </section>
             <section class="second-background-color">
-            <div class="row">
-               
-                <div class="column column-25 column-offset-20 heading">
-                    <p>
-                        <?php the_field('heading2'); ?>
-                    </p>
+                <div class="row flex-direction-row">
+
+                    <div class="column column-25 column-offset-20 heading">
+                        <p>
+                            <?php the_field('heading2'); ?>
+                        </p>
 
 
-                </div>
-                
-              
-            </div>
-            <div class="row">
-               
-              
-                <div class="column column-10 column-offset-20">
-                    <!-- Maybe use a UL list for the little circle bullets as decoration on border -->
-                    <p>Freelance</p>
-                </div>
-                <div class="column column-40">
-                    <p>Front End Developer</p>
-                </div>
-            </div>  
-            <div class="row">
-                <div class="column column-10 column-offset-20">
-                    <p>July 2014 - Present</p>
-                </div>
-                    <div class="column column-40">
-                    <p>Working on WordPress sites, etc. etc.</p>
                     </div>
-                   
-            </div>
-            <div class="row">
-               <div class="column column-100 column-offset-20">
-                    <p>Front End Developer</p>
+
+
                 </div>
-                <div class="column column-40">
-                    <p>Security Officer</p>
+                <div class="row flex-direction-row">
+
+
+                    <div class="column column-25 column-offset-10 column-border">
+                        <!-- Maybe use a UL list for the little circle bullets as decoration on border -->
+                        <p>Freelance</p>
+                    </div>
+                    <div class="column column-10">
+                        <ul class="ulborder">
+                        <li></li>
+                        </ul>
+                    </div>
+                    <div class="column column-50 column-offset-10">
+                        <p>Front End Developer</p>
+                    </div>
                 </div>
-                
-           </div>
-                </section>
-            
+                <div class="row flex-direction-row">
+                    <div class="column column-25 column-offset-10 column-border">
+                        <p>July 2014 - Present</p>
+                    </div>
+                      <div class="column column-10">
+                        <ul class="ulborder">
+                        <li></li>
+                        </ul>
+                    </div>
+                    <div class="column column-50 column-offset-10">
+                        <p>Working on WordPress sites, etc. etc. more content keep going keep going keep going</p>
+                    </div>
+
+                </div>
+                <div class="row flex-direction-row">
+                    <div class="column column-25 column-offset-10 column-border">
+                        <p>Front End Developer</p>
+                    </div>
+                      <div class="column column-10">
+                        <ul class="ulborder">
+                        <li></li>
+                        </ul>
+                    </div>
+                    <div class="column column-50 column-offset-10">
+                        <p>Security Officer</p>
+                    </div>
+
+                </div>
+            </section>
+
             <section class="bottom-background-color">
-            <div class="row">
-               
-                <div class="column column-25 column-offset-20 heading">
-                    <p>
-                        <?php the_field('heading3'); ?>
-                    </p>
+                <div class="row">
 
-                </div>
-               
-               
-            </div>
-            
-            
-            <div class="row">
-                
-                <div class="column">
-                    <p>
-                        <?php the_field('photo2'); ?>
-                    </p>
+                    <div class="column column-25 column-offset-20 heading">
+                        <p>
+                            <?php the_field('heading3'); ?>
+                        </p>
 
-                </div>
-                <div class="column">
-                    <p> description of photo....text</p>
-                </div>
-               
-            </div>
-            <div class="row">
-                
-                <div class="column column-25 column-offset-20 heading">
-                    <p>
-                        <?php the_field('heading4'); ?>
-                    </p>
+                    </div>
+
 
                 </div>
 
-                <div class="column">
-                    <p>find me links</p>
-                    <p>find me links</p>
-                    <div class="social-icon"><a href="https://twitter.com/Tyler_Co_"><i class="fa fa-lg fa-twitter" aria-hidden="true"></i></a></div>
-                    <div class="social-icon"><a href="https://github.com/temoty"><i class="fa fa-lg fa-github" aria-hidden="true"></i></a></div>
-                    <div class="social-icon"><a href="https://www.linkedin.com/in/tyler-cornett-a8a815100/"><i class="fa fa-lg fa-linkedin" aria-hidden="true"></i></a></div>
+
+                <div class="row">
+
+                    <div class="column">
+                        <p>
+                            <?php the_field('photo2'); ?>
+                        </p>
+
+                    </div>
+                    <div class="column">
+                        <p> description of photo....text</p>
+                    </div>
 
                 </div>
-               
-            </div>
+                <div class="row">
 
-           
+                    <div class="column column-25 column-offset-20 heading">
+                        <p>
+                            <?php the_field('heading4'); ?>
+                        </p>
 
-            <?php endwhile; ?>
-            <?php endif; ?>
+                    </div>
+
+                    <div class="column">
+                        <p>find me links</p>
+                        <p>find me links</p>
+                        <div class="social-icon"><a href="https://twitter.com/Tyler_Co_"><i class="fa fa-lg fa-twitter" aria-hidden="true"></i></a></div>
+                        <div class="social-icon"><a href="https://github.com/temoty"><i class="fa fa-lg fa-github" aria-hidden="true"></i></a></div>
+                        <div class="social-icon"><a href="https://www.linkedin.com/in/tyler-cornett-a8a815100/"><i class="fa fa-lg fa-linkedin" aria-hidden="true"></i></a></div>
+
+                    </div>
+
+                </div>
 
 
-            <?php wp_reset_query();
+
+                <?php endwhile; ?>
+                <?php endif; ?>
+
+
+                <?php wp_reset_query();
 				
 		?>
 
- </section>
+            </section>
 
 
         </div>
