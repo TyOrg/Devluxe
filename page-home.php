2d5567288b4302827f78f040dd7a0c93cb9ef905
temoty <temoty@hotmail.com>
2017-06-25 23:49:09 -0400

    added experience and pdf resume

--- page-home.php
+++ page-home.php
@@ -77,7 +77,7 @@
                                     
                                 <li><i class="fa fa-globe" aria-hidden="true"></i>&nbsp;&nbsp;Boston</li>
                                 <li><i class="fa fa-envelope-o" aria-hidden="true"></i>&nbsp;&nbsp;tylercornettnow [at] gmail.com</li>
-                                <li><i class="fa fa-envelope-o" aria-hidden="true"></i>&nbsp;&nbsp;Twitter</li>
+                                <li><a href="http://localhost:8888/devluxe/wp-content/uploads/2017/06/Resume.pdf"><i class="fa fa-file-text-o" aria-hidden="true"></i>&nbsp;&nbsp;Resume</a></li>
                                  </ul>    
                                 </div>
 
@@ -138,22 +138,23 @@
 
                     <div id="column-border" class="column column-25 column-offset-25 column-border">
                         <!-- Maybe use a UL list for the little circle bullets as decoration on border -->
-                        <p>Freelance</p>
+                        <p>2015-Present</p>
                         <div class="ulborder"><i class="fa fa-cog fa-spin" aria-hidden="true"></i></div>
                     </div>
 
 
                     <div class="col2ndsection column-50">
-                        <p>Front End Developer</p>
+                        <p>Front End Developer - BKJ Productions, Freelance, skilled in WordPress Development, PHP, HTML, CSS(Bootstrap, Sass), Javascript(jQuery, Node), Git, SEO, website optimization, email marketing, Pay Per Click advertising.</p>
                     </div>
                 </div>
                 <div class="row flex-direction-row">
                     <div id="column-border" class="column column-25 column-offset-25 column-border">
-                        <p>July 2014 - Present</p>
+                        <p>2016</p>
                         <div class="ulborder"><i class="fa fa-cog fa-spin" aria-hidden="true"></i></div>
                     </div>
                     <div class="column-50 col2ndsection">
-                        <p>Working on WordPress sites, etc. etc. more content keep going keep going keep going</p>
+                        <p>E-Learning Department, Lesley University, Graduate Student Assistant - Increased efficiency and performance within E-Learning Department by developing Administrative Support solutions,
+Excel Spreadsheet Reports, and Learning Management System Software edits.</p>
                     </div>
                     </div>
                    
@@ -162,11 +163,11 @@
                 
                 <div class="row flex-direction-row">
                     <div id="column-border" class="column column-25 column-offset-25 column-border">
-                        <p>Front End Developer</p>
+                        <p>1996 - 2015</p>
                         <div class="ulborder"><i class="fa fa-cog fa-spin" aria-hidden="true"></i></div>
                     </div>
                      <div class="column-50 col2ndsection">
-                        <p>Security Officer</p>
+                        <p>U.S. Coast Guard Information Systems Technician - Received 5 Coast Guard Achievement Awards while completing a 20 Year Active Duty Coast Guard career on the West and East Coast of United States as a Chief Warrant Officer.  Spent years managing large scale IT projects, served as a Windows systems administrator, supervised junior technicians, served as a junior applications developer utilizing SQL, VB Script, HTML, CSS, and Databases, and served as an IT Security Officer.</p>
                     </div>
                     </div>
                     
